7af4e72301abd845ff5ed7db36ef39215a8b4420
Shivi <ShiviMac@macbook-pro.san.rr.com>
2021-02-20 13:47:10 -0800

    really not sure what's in this one... have fun!

---
+++ finalDAPLAN/src/finalDAPLAN/TaskC.java
@@ -0,0 +1,16 @@
+package finalDAPLAN;
+
+import java.util.Comparator;
+
+public class TaskC implements Comparator<Task>{
+	@Override
+	public int compare(Task t1, Task t2){
+		return t1.getDueInHowMany() - t2.getDueInHowMany();
+	}
+	
+	
+
+
+	
+
+}
