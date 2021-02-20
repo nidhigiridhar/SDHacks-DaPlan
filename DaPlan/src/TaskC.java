e92dcc5bc094f671cb5be124bed506b345a769bc
Shivi <ShiviMac@macbook-pro.san.rr.com>
2021-02-20 12:13:39 -0800

    basically moved files idk

---
+++ DaPlan/src/TaskC.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TaskC implements Comparator<Task>{
+	@Override
+	public int compare(Task t1, Task t2){
+		return t1.getDueInHowMany() - t2.getDueInHowMany();
+	}
+
+
+	
+
+}
