f173eec8ca1ca22a3b9a685cf2a66322d22a8697
ptruong0 <philtr928@gmail.com>
2021-02-20 21:03:00 -0800

    deleted extra folders

--- DaPlan/src/TaskC.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class TaskC implements Comparator<Task>{
-	@Override
-	public int compare(Task t1, Task t2){
-		return t1.getDueInHowMany() - t2.getDueInHowMany();
-	}
-}
