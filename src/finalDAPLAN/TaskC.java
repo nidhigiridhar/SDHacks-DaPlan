c11afbdc3ecccf773de0d19cda433ab8f9ebab24
nidhigiridhar <57814964+nidhigiridhar@users.noreply.github.com>
2021-02-21 06:36:26 -0800

    Update TaskC.java with comments

--- src/finalDAPLAN/TaskC.java
+++ src/finalDAPLAN/TaskC.java
@@ -2,6 +2,10 @@ package finalDAPLAN;
 
 import java.util.Comparator;
 
+/**
+* This class implements a comparator to sort a LinkedList<Task> in ascending order of the task dueInHowMany.
+* (sorted based on earliest due date to latest due date) 
+*/
 public class TaskC implements Comparator<Task>{
 	@Override
 	public int compare(Task t1, Task t2){
